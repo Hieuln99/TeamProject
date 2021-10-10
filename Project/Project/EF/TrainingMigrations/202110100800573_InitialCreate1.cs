a10c7502c9e062e1bc51504cfca889f419d483cb
hieutroll99 <hieutroll99@gmail.com>
2021-10-10 15:02:29 +0700

    new

---
+++ Project/Project/EF/TrainingMigrations/202110100800573_InitialCreate1.cs
@@ -0,0 +1,18 @@
+﻿namespace Project.EF.TrainingMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Trainer", "type", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Trainer", "type", c => c.Boolean(nullable: false));
+        }
+    }
+}
