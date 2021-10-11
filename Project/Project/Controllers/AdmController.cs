aff5582c7b3df34bfb4268e06dc1eb60ac830f90
hieutroll99 <hieutroll99@gmail.com>
2021-10-11 19:01:46 +0700

    manage staff

--- Project/Project/Controllers/AdmController.cs
+++ Project/Project/Controllers/AdmController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,35 +15,163 @@ namespace Project.Controllers
             return View();
         }
 
+
+        public ActionResult TrainerAcc()
+        {
+            using(var TNCT = new EF.TrainingContext())
+            {
+                var trainers = TNCT.trainers.OrderBy(t => t.id).ToList();
+                return View(trainers);
+            }
+        }
+
+
+        [HttpGet]
         public ActionResult AddTrainerAcc()
         {
             return View();
         }
-        public ActionResult EditTrainerAcc()
+        [HttpPost]
+        public ActionResult AddTrainerAcc(Trainer t)
         {
-            return View();
-        }
-        public ActionResult DeleteTrainerAcc()
-        {
-            return View();
+            validation(t);
+            if (!ModelState.IsValid)
+            {
+                return View(t);
+            }
+            else
+            {
+                using (var TNCT = new EF.TrainingContext())
+                {
+                    TNCT.trainers.Add(t);
+                    TNCT.SaveChanges();
+                }
+            }
+            TempData["message"] = $"Add Successfully a trainer with id: {t.id}";
+            return RedirectToAction("TrainerAcc");
         }
 
+
+        [HttpGet]
+        public ActionResult EditTrainerAcc(int id)
+        {
+            using (var TNCT = new EF.TrainingContext())
+            {
+                var trainers = TNCT.trainers.FirstOrDefault(t => t.id == id);
+                if (trainers == null)
+                {
+                    return RedirectToAction("TrainerAcc");
+                }
+                else
+                {
+                    return View(trainers);
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult EditTrainerAcc(int id, Trainer t)
+        {
+            validation(t);
+            if (!ModelState.IsValid)
+            {
+                return View(t);
+            }
+            else
+            {
+                using (var TNCT = new EF.TrainingContext())
+                {
+                    TNCT.Entry<Trainer>(t).State = System.Data.Entity.EntityState.Modified;
+                    TNCT.SaveChanges();
+                }
+                TempData["message"] = $"Edit successfully a trainer with id: {t.id}";
+                return RedirectToAction("TrainerAcc");
+            }
+        }
+
+
+        public ActionResult DeleteTrainerAcc(int id)
+        {
+            using (var TNCT = new EF.TrainingContext())
+            {
+                var t = TNCT.trainers.FirstOrDefault(u => u.id == id);
+                if (t == null)
+                {
+                    TempData["message"] = $"Delete failed";
+                }
+                TNCT.trainers.Remove(t);
+                TNCT.SaveChanges();
+
+                TempData["message"] = $"Delete successfully a trainer with id: {t.id}";
+            }
+            return RedirectToAction("TrainerIndex");
+        }
+
+        private void validation(Trainer t)
+        {
+            if (!string.IsNullOrEmpty(t.phonenumber) && t.phonenumber[0] != '0')
+            {
+                ModelState.AddModelError("Name", "Phone number must start with 0");
+            }
+        }
 
 
         //-----------------------------------
+        public ActionResult StaffAcc()
+        {
+            using(var TNCT = new EF.TrainingContext())
+            {
+                var staffs = TNCT.staffs.OrderBy(s => s.id).ToList();
+                return View(staffs);
+            }
+        }
+
+        [HttpGet]
         public ActionResult AddStaffAcc()
         {
             return View();
         }
-        public ActionResult EditStaffAcc()
+        [HttpPost]
+        public ActionResult AddStaffAcc(Staff s)
+        {
+            
+                using (var TNCT = new EF.TrainingContext())
+                {
+                    TNCT.staffs.Add(s);
+                    TNCT.SaveChanges();
+                }
+            
+            TempData["message"] = $"Add Successfully a staff with id: {s.id}";
+            return RedirectToAction("TrainerAcc");
+        }
+
+        [HttpGet]
+        public ActionResult EditStaffAcc(int id)
+        {
+            using (var TNCT = new EF.TrainingContext())
+            {
+                var staffs = TNCT.staffs.FirstOrDefault(s => s.id == id);
+                if (staffs == null)
+                {
+                    return RedirectToAction("StaffAcc");
+                }
+                else
+                {
+                    return View(staffs);
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult EditStaffAcc(Staff s)
         {
             return View();
         }
+
         public ActionResult DeleteStaffAcc()
         {
             return View();
         }
 
+      
 
     }
 }
---
+++ Project/Project/EF/TrainingMigrations/202110111132357_InitialCreate4.cs
@@ -0,0 +1,38 @@
+﻿namespace Project.EF.TrainingMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate4 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Staff",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                        username = c.String(nullable: false, maxLength: 50, unicode: false),
+                        password = c.String(nullable: false, maxLength: 50, unicode: false),
+                    })
+                .PrimaryKey(t => t.id);
+
+
+
+            Sql("INSERT INTO dbo.Staff( name, username, password) VALUES" +
+               "('Mary','JoiMaguire', '0919919999')," +
+                "('Mary', 'Maguire','0919919999')," +
+                "('Mary', 'Maguire','0919919999')," +
+                "('Mary', 'Maguire','0919919999')," +
+                "('Mary', 'Maguire','0919919999')," +
+                "('Mary', 'Maguire','0919919999')," +
+                "('Tobey Maguire','Tobey','0919919999')");
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Staff");
+        }
+    }
+}
