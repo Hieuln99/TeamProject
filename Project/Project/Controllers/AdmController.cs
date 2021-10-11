bd6d49279a256f03a169912909ad75ec2ffda9f7
hieutroll99 <hieutroll99@gmail.com>
2021-10-11 16:50:27 +0700

    admin

---
+++ Project/Project/Controllers/AdmController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class AdmController : Controller
+    {
+        // GET: Adm
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+
+        public ActionResult AddTrainerAcc()
+        {
+            return View();
+        }
+        public ActionResult EditTrainerAcc()
+        {
+            return View();
+        }
+        public ActionResult DeleteTrainerAcc()
+        {
+            return View();
+        }
+
+
+
+        //-----------------------------------
+        public ActionResult AddStaffAcc()
+        {
+            return View();
+        }
+        public ActionResult EditStaffAcc()
+        {
+            return View();
+        }
+        public ActionResult DeleteStaffAcc()
+        {
+            return View();
+        }
+
+
+    }
+}
