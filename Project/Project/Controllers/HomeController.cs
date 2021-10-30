4caacb6d59b5201ed9e12cb445d75a305d499253
Rileywon <thangntgch190892@fpt.edu.vn>
2021-10-30 20:48:32 +0700

    cm

--- Project/Project/Controllers/HomeController.cs
+++ Project/Project/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace Project.Controllers
         {
             return View();
         }
+
+        public ActionResult Index222()
+        {
+            return View();
+        }
     }
 }
