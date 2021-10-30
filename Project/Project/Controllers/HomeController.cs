c2ad15aff2c9bc71b6f0ee8331b5b6d73f294a43
Rileywon <thangntgch190892@fpt.edu.vn>
2021-10-30 20:47:43 +0700

    commit

--- Project/Project/Controllers/HomeController.cs
+++ Project/Project/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Project.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index1()
         {
             return View();
         }
